092881c08f7b3719b5442f6fe0ce256adc2b2e55
Jonathan Block <block.jon@gmail.com>
2020-08-04 03:28:55 -0400

    Adjustments

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -euox pipefail
-aws s3 cp ./dist/bundle.js s3://www.jonathanblock.com/zodiac-340-solver/dist/bundle.js --acl public-read
-aws s3 cp ./index.html s3://www.jonathanblock.com/zodiac-340-solver/index.html --acl public-read
-aws s3 sync ./css s3://www.jonathanblock.com/zodiac-340-solver/css --acl public-read
+aws s3 cp ./dist/bundle.js s3://www.jonathanblock.com/zodiac/dist/bundle.js --acl public-read
+aws s3 cp ./index.html s3://www.jonathanblock.com/zodiac/index.html --acl public-read
+aws s3 sync ./css s3://www.jonathanblock.com/zodiac/css --acl public-read
