3786bc68424423c89867da25152919a06f70de86
Jonathan Block <block.jon@gmail.com>
2020-08-05 00:15:11 -0400

    Show bigrams

--- src/CipherBoard.ts
+++ src/CipherBoard.ts
@@ -6,6 +6,7 @@ class CipherBoard {
   columnOrder: Array<number> = [];
   data: Array<Array<string>> = [];
   zodiacCharLocations: { [key: string]: Array<Array<number>> } = {};
+  cipherStats: any = {};
   constructor() {
 
   }
@@ -48,6 +49,101 @@ class CipherBoard {
       row = tbody.append(row);
     }
     this.cacheZodiacCharLocations()
+    this.cipherStats = this.detectStats()
+
+    $("#unique-bigrams").text(this.cipherStats.uniqueBigrams)
+    $("#bigram-count").text(this.cipherStats.bigramCount)
+    for (let key of Object.keys(this.cipherStats.bigrams)) {
+      let value = this.cipherStats.bigrams[key]
+      for (let i = 0; i < value.length; i++) {
+        let column = value[i][0]
+        let row = value[i][1]
+        console.log(`row = ${row} column = ${column}`)
+        $("#cipher-board tbody tr").eq(row).find('td').eq(column).addClass('bigram-cell')
+      }
+    }
+  }
+  detectStats() {
+    let bigrams: { [key: string]: Array<Array<number>> } = {};
+    let cipherString = ''
+    let uniqueBigrams = 0
+    let bigramCount = 0
+    for (let i = 0; i < this.data.length; i++) {
+      cipherString += this.data[i].join('')
+    }
+    cipherString = cipherString.trim()
+    let cipherWidth: number = this.data[0].length
+    for (let i = 0; i < cipherString.length; i++) {
+      if (i + 1 == cipherString.length) {
+        break
+      }
+      let char = String(cipherString[i])
+      let nextChar = String(cipherString[i + 1])
+      let bigram = char + nextChar
+      if (bigram in bigrams) {
+        continue
+      }
+      let numOccurances = this.occurrences(cipherString, bigram)
+      if (numOccurances > 1) {
+        if (bigram in bigrams == false) {
+          bigrams[bigram] = []
+        }
+        uniqueBigrams++
+        let startSearchFrom = -1
+        while (true) {
+          let nextLocation: number = cipherString.indexOf(bigram, startSearchFrom)
+          if (nextLocation == -1) {
+            break
+          }
+          bigramCount++
+          console.log(`nextLocation = ${nextLocation}`)
+          let firstCharCoords: any = this.cipherOffsetToCoords(nextLocation)
+          bigrams[bigram].push([firstCharCoords.column, firstCharCoords.row])
+          let secondCharCoords: any = this.cipherOffsetToCoords(nextLocation + 1)
+          bigrams[bigram].push([secondCharCoords.column, secondCharCoords.row])
+          startSearchFrom = nextLocation + 2
+        }
+      }
+    }
+    return {
+      bigrams: bigrams,
+      uniqueBigrams: uniqueBigrams,
+      bigramCount: bigramCount
+    }
+  }
+  cipherOffsetToCoords(offset: number) {
+    let cipherWidth: number = this.data[0].length
+    let row = Math.floor(offset / cipherWidth)
+    let column = offset % cipherWidth
+    return { column: column, row: row }
+  }
+  /** Function that count occurrences of a substring in a string;
+   * @param {String} string               The string
+   * @param {String} subString            The sub string to search for
+   * @param {Boolean} [allowOverlapping]  Optional. (Default:false)
+   *
+   * @author Vitim.us https://gist.github.com/victornpb/7736865
+   * @see Unit Test https://jsfiddle.net/Victornpb/5axuh96u/
+   * @see http://stackoverflow.com/questions/4009756/how-to-count-string-occurrence-in-string/7924240#7924240
+   */
+  occurrences(input: string, subString: string) {
+
+    input += "";
+    subString += "";
+    if (subString.length <= 0) return (input.length + 1);
+
+    var n = 0,
+      pos = 0,
+      step = subString.length;
+
+    while (true) {
+      pos = input.indexOf(subString, pos);
+      if (pos >= 0) {
+        ++n;
+        pos += step;
+      } else break;
+    }
+    return n;
   }
   cacheZodiacCharLocations() {
     this.zodiacCharLocations = {}
