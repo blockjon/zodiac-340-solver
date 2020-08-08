fb84fcc1a289c465e1f612ca09f2596a26661a9d
Jonathan Block <block.jon@gmail.com>
2020-08-08 16:27:04 -0400

    Invalidate cf on deploy

--- deploy.sh
+++ deploy.sh
@@ -3,3 +3,4 @@ set -euox pipefail
 aws s3 cp ./dist/bundle.js s3://www.jonathanblock.com/zodiac/dist/bundle.js --acl public-read
 aws s3 cp ./index.html s3://www.jonathanblock.com/zodiac/index.html --acl public-read
 aws s3 sync ./css s3://www.jonathanblock.com/zodiac/css --acl public-read
+aws cloudfront create-invalidation --distribution-id E24D7QOY0ABOB2 --paths '/*'
